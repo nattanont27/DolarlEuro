bef3a671aa9d4f2922700cce9f20045b141d5ed2
sky indigo blue <52160323+nattanont27@users.noreply.github.com>
2019-07-02 23:26:25 +0700

    Add files via upload

---
+++ Money.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std ;
+int main () {
+	int x ;
+	cout << " Input Thai Baths : " ;
+	cin >> x ;
+	cout << "*****ExchangeRate*****" << endl ;
+	cout << x << " Baths = " << x*0.033 << " dollars" << endl ;
+	cout << x << " Baths = " << x*0.029 << " euros" << endl ;
+}
