0431584c4a85ff8406a7a73b6edd72f275c79dfc
sky indigo blue <52160323+nattanont27@users.noreply.github.com>
2020-08-17 23:21:25 +0700

    Add files via upload

---
+++ 6306021610101.cpp
@@ -0,0 +1,27 @@
+﻿//Calculate age from year born (BE)
+#include <iostream>
+using namespace std;
+int calAge(int); //Create func
+int yearCount;
+int yearIn;
+int thisYear = 2563;
+int main(){
+    cout << "Welcome to MUTIPLE age calculate program" << endl << endl;
+    cout << "Please determine how many year you want to calculate[BE]: ";  //Year insert count
+        cin >> yearCount;
+    cout << "-----------------------------------------------------------" << endl;
+        //Year Loop
+        for (int i=1; i<= yearCount; i++)
+        {
+            cout << "Insert year " << i << ": ";
+                cin >> yearIn;
+            cout << "Age " << i << ": " << calAge(yearIn) << endl; //this is OUTPUT
+        }
+    cout << endl << "Thank you." << endl;
+    system("pause");
+    return 0;
+}
+//This is calAge func.
+int calAge(int yearIn){
+    thisYear = 2563 - yearIn;
+    return thisYear;
